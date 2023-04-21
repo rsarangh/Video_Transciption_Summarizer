1a8d50e3275137648fa1360a3974fe4acc3b9265
Sarangh Ramesh KV <rsarangh@gmail.com>
2023-04-21 12:30:31 +0530

    Commit files

---
+++ VideoTranscriptionSummarizer_Android/app.py
@@ -0,0 +1,18 @@
+from flask import Flask,request,jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello Sarangh!!"
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    video_id = request.form.get('video_id')
+
+    result = {'video_id':video_id}
+
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
