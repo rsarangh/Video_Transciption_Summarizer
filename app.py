5a03ed2fd0371b353576eeb58afdf1de723c6db5
Sarangh Ramesh KV <rsarangh@gmail.com>
2023-04-22 10:23:09 +0530

    commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
 import pandas as pd
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 from youtube_transcript_api import YouTubeTranscriptApi
@@ -14,43 +14,34 @@ l = []
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=['POST'])
 def hello_world():
-    # global movie_nameee
 
-    if request.method == "POST":
-        myDict = request.form
-        # title = request.form.get('title')
-        title = (myDict['You-Tube video Id'])
+    video_id = request.form.get('video_id')
 
-        tokenizer = PegasusTokenizer.from_pretrained("google/pegasus-xsum")
-        #model = PegasusForConditionalGeneration.from_pretrained("google/pegasus-xsum")
-        av = YouTubeTranscriptApi.get_transcript(title, languages=['en'])
-        t = []
-        for m in av:
-            main_txt = (m['text'])
-            t.append(main_txt)
+    tokenizer = PegasusTokenizer.from_pretrained("google/pegasus-xsum")
 
-        l = []
-        for m in t:
-            s = list(m)
-            ln = []
-            for n in s:
-                if n == '\xa0' or n == '\n':
-                    ln.append('')
-                else:
-                    ln.append(n)
-            m = ''.join(ln)
-            l.append(m)
+    transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+    t = []
+    for m in transcript:
+        main_txt = (m['text'])
+        t.append(main_txt)
 
-        for_this = ' '.join(l)
-        #text = for_this
-        #tokens = tokenizer(for_this, truncation=True, padding="longest", return_tensors="pt")
-        #summary = model.generate(**tokens)
+    linez = []
+    for m in t:
+        s = list(m)
+        ln = []
+        for n in s:
+            if n == '\xa0' or n == '\n':
+                ln.append('')
+            else:
+                ln.append(n)
+        m = ''.join(ln)
+        linez.append(m)
 
+    for_this = ' '.join(linez)
 
-        #
-        tf_idf_vectorizer = TfidfVectorizer(min_df=2, max_features=None,
+    tf_idf_vectorizer = TfidfVectorizer(min_df=2, max_features=None,
                                             strip_accents='unicode',
                                             analyzer='word',
                                             token_pattern=r'\w{1,}',
@@ -59,30 +50,22 @@ def hello_world():
                                             sublinear_tf=1,
                                             stop_words='english')
 
-        tf_idf_vectorizer.fit(l)
-        sentence_vectors = tf_idf_vectorizer.transform(l)
+    tf_idf_vectorizer.fit(linez)
+    sentence_vectors = tf_idf_vectorizer.transform(linez)
 
-        # Getting sentence scores for each sentences
-        sentence_scores = np.array(sentence_vectors.sum(axis=1)).ravel()
+    # Getting sentence scores for each sentences
+    sentence_scores = np.array(sentence_vectors.sum(axis=1)).ravel()
 
-        # Sanity checkup
-        print(len(l) == len(sentence_scores))
-        N = 5
-        top_n_sentences = [l[ind] for ind in np.argsort(sentence_scores, axis=0)[::-1][:N]]
-
-
+    # Sanity checkup
+    print(len(linez) == len(sentence_scores))
+    N = 5
+    top_n_sentences = [linez[ind] for ind in np.argsort(sentence_scores, axis=0)[::-1][:N]]
 
 
+    result = ' '.join(top_n_sentences)
 
+    return jsonify({'summary': result})
 
-
-        #mm = tokenizer.decode(summary[0])
-        #top_n_sentences.append(mm)
-        qqq = ' '.join(top_n_sentences)
-        # print(qqq)
-        return render_template('show.html', inf=qqq)
-
-    return render_template('kd.html')
 
 
 if __name__ == "__main__":
